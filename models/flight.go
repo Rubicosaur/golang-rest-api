fa9c375a08482878483673cd96dd000f0cb35b56
Wojciech Walczysko <wojwal94@gmail.com>
2022-07-16 18:49:55 +0200

    Added flight model

--- models/flight.go
+++ models/flight.go
@@ -1,4 +1,8 @@
 package models
 
-
-type 
+type Flight struct {
+	Id                string `json:"id"`
+	PlaneModel        string `json:"planemodel"`
+	DepartureLocation string `json:"departurelocation"`
+	ArrivalLocation   string `json:"arrivallocation"`
+}
